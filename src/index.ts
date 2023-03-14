37c16227dc80f0a08f0739e070a118a755833e7b
BiscuitCleaner <bobpopreal@outlook.com>
2023-03-14 22:39:30 +0800

    make dontputcloudinventory in bot.ci

--- src/index.ts
+++ src/index.ts
@@ -18,18 +18,17 @@ declare module 'mineflayer' {
             disable: () => void
             enable: () => void
             put: () => void
+            dontputcloudinventory: boolean
         }
     }
 
     interface BotEvents {
         ci_startputting: (putItems: Item[]) => void
         ci_finishedputting: (putItems: Item[]) => void
-        ci_error: (error?: Error) => void
     }
 }
 
 // 防止物品存入云仓的速度太快，导致反作弊踢出
-var dontputcloudinventory = false
 export function plugin(bot: mineflayer.Bot){
     bot.ci.disabled = true
     bot.ci.isPutting = false
@@ -53,7 +52,7 @@ export function plugin(bot: mineflayer.Bot){
     }
     bot.on("physicTick",() => {
         // 检查是否已经超越速度限制
-        if (dontputcloudinventory) return
+        if (bot.ci.dontputcloudinventory) return
         // 检查是否已经进行了存入，如果是则返回
         if (bot.ci.isPutting) return
         // 提取所有需要存入云仓的物品
@@ -61,13 +60,14 @@ export function plugin(bot: mineflayer.Bot){
         // 检查需要存入云仓的物品组是否超过或等于最少放置多少组物品的数量
         if (put_items.length >= bot.ci.options.minimum_stack){
             bot.ci.isPutting = true
-            dontputcloudinventory=true
+            bot.ci.dontputcloudinventory=true
             bot.chat("/ci put")
             bot.emit("ci_startputting", put_items)
         }
     })
 
     bot.on("windowOpen", (window) => {
+        bot.ci.isPutting = true
         // 检查窗口标题是否云仓专属的，如果不是则返回
         if (window.title !== "{\"text\":\"上传物品\"}") return
 
@@ -99,7 +99,7 @@ export function plugin(bot: mineflayer.Bot){
         bot.emit("ci_finishedputting", put_items)
         // 解除速度限制
         setTimeout(() => {
-            dontputcloudinventory=false
+            bot.ci.dontputcloudinventory=false
         }, 2500)
     })
 }
